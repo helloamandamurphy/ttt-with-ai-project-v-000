e154c3f29f24314127634658499535f6ab4a290d
Amanda Murphy <helloamandamurphy@gmail.com>
2018-11-23 17:21:19 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -12,11 +12,11 @@ module Players
       elsif board.turn_count > 2
         Game::WIN_COMBINATIONS.detect do |combo|
             if board.cells[combo[0]] == board.cells[combo[1]] && board.taken?(combo[0] + 1)
-              move = board.cells[combo[2]].first
+              move = board.cells[combo[2]]
             elsif board.cells[combo[0]] == board.cells[combo[2]] && board.taken?(combo[0] + 1)
-              move = board.cells[combo[1]].first
+              move = board.cells[combo[1]]
             elsif board.cells[combo[1]] == board.cells[combo[2]] && board.taken?(combo[1] + 1)
-              move = board.cells[combo[0]].first
+              move = board.cells[combo[0]]
             elsif
               move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
             else
